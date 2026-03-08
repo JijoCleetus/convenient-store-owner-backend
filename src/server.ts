451dee35a1a723510ce738a7cc1e4688de0c6742
JijoCleetus <jijocleetus@gmail.com>
2026-03-08 00:54:38 +0000

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Initialize middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://69acc7ff547b70cc281305b6--stirring-centaur-0e1025.netlify.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
